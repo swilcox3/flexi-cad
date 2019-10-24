f591fd1a7b7ba2c8487040e2e2633541396628a6
Scot Wilcox <SWilcox@nemetschek.net>
2019-10-24 16:24:48 -0400

    Attempt to add textures.  Didn't work.

--- ui/src/rendering/renderer.ts
+++ ui/src/rendering/renderer.ts
@@ -1,11 +1,11 @@
 import * as BABYLON from 'babylonjs'
+import * as TEXTURES from './babylonjs.proceduralTextures.min.js'
 import * as mouse from '../ui/mouse_events'
 import * as gui from '../ui/gui'
-import {UIControllerSingleton} from '../ui/controller'
+import { UIControllerSingleton } from '../ui/controller'
 
-function getHoveredMesh(scene: BABYLON.Scene, ground: BABYLON.Mesh)
-{
-    var pickinfo = scene.pick(scene.pointerX, scene.pointerY, mesh => { return mesh != ground});
+function getHoveredMesh(scene: BABYLON.Scene, ground: BABYLON.Mesh) {
+    var pickinfo = scene.pick(scene.pointerX, scene.pointerY, mesh => { return mesh != ground });
     if (pickinfo.hit) {
         return pickinfo.pickedMesh;
     }
@@ -55,8 +55,8 @@ export class Renderer {
             if (current_hover && hovered != current_hover) {
                 layer.removeMesh(current_hover)
             }
-            if(mouse.onPointerMove(this._scene, ground, hovered as BABYLON.Mesh)) {
-                if(hovered) {
+            if (mouse.onPointerMove(this._scene, ground, hovered as BABYLON.Mesh)) {
+                if (hovered) {
                     layer.addMesh(hovered as BABYLON.Mesh, BABYLON.Color3.Green());
                     current_hover = hovered as BABYLON.Mesh;
                 }
@@ -64,7 +64,7 @@ export class Renderer {
         }
 
         this._scene.onPointerObservable.add((pointerInfo) => {
-            switch(pointerInfo.type) {
+            switch (pointerInfo.type) {
                 case BABYLON.PointerEventTypes.POINTERDOWN:
                     break;
                 case BABYLON.PointerEventTypes.POINTERUP:
@@ -85,7 +85,7 @@ export class Renderer {
         });
     }
     initialize(canvas: HTMLCanvasElement) {
-        const engine = new BABYLON.Engine(canvas, true, {stencil: true});
+        const engine = new BABYLON.Engine(canvas, true, { stencil: true });
         this.createScene(canvas, engine);
         engine.runRenderLoop(() => {
             this._scene.render();
@@ -99,19 +99,23 @@ export class Renderer {
         this._engine.stopRenderLoop();
     }
 
-    applyNewMeshProps(mesh: BABYLON.Mesh, temp?:boolean) {
-        if(!temp) {
-            var objMaterial = new BABYLON.StandardMaterial("obj", this._scene);
+    applyNewMeshProps(mesh: BABYLON.Mesh, temp?: boolean) {
+        if (!temp) {
+            var objMaterial = new BABYLON.StandardMaterial("obj", this._scene)
+            /*var woodTexture = new TEXTURES.WoodProceduralTexture("wood", 1024, this._scene);
+            woodTexture.ampScale = 80.0;
+            woodTexture.woodColor = BABYLON.Color3.Red();
+            objMaterial.diffuseTexture = woodTexture;*/
             objMaterial.diffuseColor = BABYLON.Color3.Gray();
             objMaterial.backFaceCulling = false;
             mesh.material = objMaterial;
-            var pointerDragBehavior = new BABYLON.PointerDragBehavior({dragPlaneNormal: new BABYLON.Vector3(0,1,0)});
+            var pointerDragBehavior = new BABYLON.PointerDragBehavior({ dragPlaneNormal: new BABYLON.Vector3(0, 1, 0) });
             pointerDragBehavior.useObjectOrienationForDragging = false;
             var uiSingleton = new UIControllerSingleton().getInstance();
-            pointerDragBehavior.onDragObservable.add((ev:any)=>{
+            pointerDragBehavior.onDragObservable.add((ev: any) => {
                 uiSingleton.objDrag(ev, mesh)
             })
-            pointerDragBehavior.onDragEndObservable.add((ev:any)=>{
+            pointerDragBehavior.onDragEndObservable.add((ev: any) => {
                 uiSingleton.objDragEnd(ev)
             })
             pointerDragBehavior.moveAttached = false;
@@ -125,9 +129,9 @@ export class Renderer {
         }
     }
 
-    renderMesh(triangles: any, id: string, temp?:boolean) {
+    renderMesh(triangles: any, id: string, temp?: boolean) {
         var mesh = this._scene.getMeshByName(id) as BABYLON.Mesh
-        if(!mesh) {
+        if (!mesh) {
             mesh = new BABYLON.Mesh(id, this._scene);
             this.applyNewMeshProps(mesh, temp);
         }
@@ -138,7 +142,24 @@ export class Renderer {
         vertexData.applyToMesh(mesh);
     }
 
-    renderObject(json: any, id: string, temp?:boolean) {
+    showNormals(mesh: BABYLON.Mesh) {
+        var normals = mesh.getVerticesData(BABYLON.VertexBuffer.NormalKind);
+        var positions = mesh.getVerticesData(BABYLON.VertexBuffer.PositionKind);
+        var color = BABYLON.Color3.White();
+        var size = 1;
+
+        var lines = [];
+        for (var i = 0; i < normals.length; i += 3) {
+            var v1 = BABYLON.Vector3.FromArray(positions, i);
+            var v2 = v1.add(BABYLON.Vector3.FromArray(normals, i).scaleInPlace(size));
+            lines.push([v1.add(mesh.position), v2.add(mesh.position)]);
+        }
+        var normalLines = BABYLON.MeshBuilder.CreateLineSystem("normalLines", { lines: lines }, this._scene);
+        normalLines.color = color;
+        return normalLines;
+    }
+
+    renderObject(json: any, id: string, temp?: boolean) {
         var mesh = this._scene.getMeshByName(id) as BABYLON.Mesh
         switch (json.metadata.type) {
             case "Dimension":
@@ -148,28 +169,28 @@ export class Renderer {
                 var second_off = new BABYLON.Vector3(json.second_off.x, 1, json.second_off.z);
                 var text_pos = new BABYLON.Vector3(json.text_pos.x, 1, json.text_pos.z);
 
-                if(!mesh) {
-                    mesh = BABYLON.MeshBuilder.CreateSphere(id, {diameter: 2}, this._scene);
+                if (!mesh) {
+                    mesh = BABYLON.MeshBuilder.CreateSphere(id, { diameter: 2 }, this._scene);
                 }
                 this.applyNewMeshProps(mesh, temp);
                 mesh.metadata = json.metadata;
                 mesh.position = text_pos;
 
                 var line_1 = this._scene.getMeshByName(id + "_line1");
-                if(!line_1) {
+                if (!line_1) {
                     var line_1_pts = [first, first_off];
-                    line_1 = BABYLON.MeshBuilder.CreateLines(id + "_line1", {points: line_1_pts, updatable: true}, this._scene)
+                    line_1 = BABYLON.MeshBuilder.CreateLines(id + "_line1", { points: line_1_pts, updatable: true }, this._scene)
                 }
                 else {
                     var positions = line_1.getVerticesData(BABYLON.VertexBuffer.PositionKind);
                     positions = [first.x, first.y, first.z, first_off.x, first_off.y, first_off.z]
                     line_1.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
                 }
-                
+
                 var line_2 = this._scene.getMeshByName(id + "_line2");
-                if(!line_2) {
+                if (!line_2) {
                     var line_2_pts = [second, second_off];
-                    line_2 = BABYLON.MeshBuilder.CreateLines(id + "_line2", {points: line_2_pts, updatable: true}, this._scene)
+                    line_2 = BABYLON.MeshBuilder.CreateLines(id + "_line2", { points: line_2_pts, updatable: true }, this._scene)
                 }
                 else {
                     var positions = line_2.getVerticesData(BABYLON.VertexBuffer.PositionKind);
@@ -177,9 +198,9 @@ export class Renderer {
                     line_2.updateVerticesData(BABYLON.VertexBuffer.PositionKind, positions);
                 }
                 var line_3 = this._scene.getMeshByName(id + "_line3");
-                if(!line_3) {
+                if (!line_3) {
                     var line_3_pts = [first_off, second_off];
-                    line_3 = BABYLON.MeshBuilder.CreateLines(id + "_line3", {points: line_3_pts, updatable: true}, this._scene)
+                    line_3 = BABYLON.MeshBuilder.CreateLines(id + "_line3", { points: line_3_pts, updatable: true }, this._scene)
                 }
                 else {
                     var positions = line_3.getVerticesData(BABYLON.VertexBuffer.PositionKind);
@@ -190,26 +211,26 @@ export class Renderer {
         }
     }
 
-    deleteMesh(id:string) {
+    deleteMesh(id: string) {
         var mesh = this._scene.getMeshByName(id)
-        if(mesh) {
+        if (mesh) {
             mesh.dispose()
         }
         var mesh = this._scene.getMeshByName(id + "_line1")
-        if(mesh) {
+        if (mesh) {
             mesh.dispose()
         }
         var mesh = this._scene.getMeshByName(id + "_line2")
-        if(mesh) {
+        if (mesh) {
             mesh.dispose()
         }
         var mesh = this._scene.getMeshByName(id + "_line3")
-        if(mesh) {
+        if (mesh) {
             mesh.dispose()
         }
     }
 
-    getMesh(id:string) {
+    getMesh(id: string) {
         return this._scene.getMeshByName(id) as BABYLON.Mesh
     }
 }
--- ui/src/ui/controller.ts
+++ ui/src/ui/controller.ts
@@ -1,18 +1,16 @@
 import * as gui from './gui'
 import * as BABYLON from 'babylonjs'
-import {Point3d, transformGraphicToModelCoords} from '../utils/math'
+import { Point3d, transformGraphicToModelCoords } from '../utils/math'
 import * as ops from "../operations/operations"
 
-interface Tool
-{
-    onMouseDown(pt: Point3d, hovered?:BABYLON.Mesh):void,
-    onMouseMove(pt: Point3d, hovered?:BABYLON.Mesh):boolean,
-    cancel():void,
-    finish(pt: Point3d, picked?:BABYLON.Mesh):void
+interface Tool {
+    onMouseDown(pt: Point3d, hovered?: BABYLON.Mesh): void,
+    onMouseMove(pt: Point3d, hovered?: BABYLON.Mesh): boolean,
+    cancel(): void,
+    finish(pt: Point3d, picked?: BABYLON.Mesh): void
 }
 
-class SelectionController
-{
+class SelectionController {
     private selectedObjs: Set<BABYLON.Mesh>;
     ctrlPressed: boolean;
     constructor() {
@@ -20,18 +18,15 @@ class SelectionController
         this.ctrlPressed = false;
     }
 
-    getSelectedObjs()
-    {
+    getSelectedObjs() {
         return this.selectedObjs
     }
 
-    isSelected(mesh: BABYLON.Mesh)
-    {
+    isSelected(mesh: BABYLON.Mesh) {
         return this.selectedObjs.has(mesh)
     }
 
-    deselectAll()
-    {
+    deselectAll() {
         this.selectedObjs.forEach((obj) => {
             var mat = obj.material as BABYLON.StandardMaterial;
             mat.diffuseColor = BABYLON.Color3.Gray();
@@ -39,23 +34,20 @@ class SelectionController
         this.selectedObjs.clear();
     }
 
-    addObject(mesh: BABYLON.Mesh)
-    {
+    addObject(mesh: BABYLON.Mesh) {
         var mat = mesh.material as BABYLON.StandardMaterial;
         mat.diffuseColor = BABYLON.Color3.Green();
         this.selectedObjs.add(mesh)
     }
 
-    removeObject(mesh: BABYLON.Mesh)
-    {
+    removeObject(mesh: BABYLON.Mesh) {
         var mat = mesh.material as BABYLON.StandardMaterial;
         mat.diffuseColor = BABYLON.Color3.Gray();
         this.selectedObjs.delete(mesh);
     }
 
-    deleteSelected()
-    {
-        if(this.selectedObjs.size > 0) {
+    deleteSelected() {
+        if (this.selectedObjs.size > 0) {
             var event = ops.beginUndoEvent("Delete objs");
             this.selectedObjs.forEach((obj) => {
                 ops.deleteObject(event, obj.name)
@@ -65,11 +57,9 @@ class SelectionController
         }
     }
 
-    selectObj(mesh: BABYLON.Mesh)
-    {
-        if(!this.isSelected(mesh))
-        {
-            if(!this.ctrlPressed) {
+    selectObj(mesh: BABYLON.Mesh) {
+        if (!this.isSelected(mesh)) {
+            if (!this.ctrlPressed) {
                 this.deselectAll();
                 this.addObject(mesh);
             }
@@ -79,15 +69,14 @@ class SelectionController
             gui.guiInstance.setObjectOverlay(this.selectedObjs)
         }
         else {
-            if(this.ctrlPressed) {
+            if (this.ctrlPressed) {
                 this.removeObject(mesh)
             }
         }
     }
 }
 
-class MoveObjectsController
-{
+class MoveObjectsController {
     private delta: Point3d;
     private real_to_temp_objs: Map<string, ops.DataObject>;
     constructor() {
@@ -95,9 +84,8 @@ class MoveObjectsController
         this.real_to_temp_objs = null;
     }
 
-    move(ev: any, objs: Set<BABYLON.Mesh>)
-    {
-        if(this.delta == null) {
+    move(ev: any, objs: Set<BABYLON.Mesh>) {
+        if (this.delta == null) {
             this.real_to_temp_objs = new Map();
             objs.forEach((mesh) => {
                 var temp = ops.createDataObjectFromJSON(mesh.metadata);
@@ -120,9 +108,8 @@ class MoveObjectsController
         }
     }
 
-    endMove(ev: any)
-    {
-        if(this.delta && this.real_to_temp_objs) {
+    endMove(ev: any) {
+        if (this.delta && this.real_to_temp_objs) {
             const event = ops.beginUndoEvent("Move objs");
             var promise = ops.moveObjs(event, Array.from(this.real_to_temp_objs.keys()), this.delta);
             ops.endUndoEvent(event)
@@ -137,8 +124,7 @@ class MoveObjectsController
     }
 }
 
-class UIController
-{
+class UIController {
     private activeTool: Tool
     private selection: SelectionController
     private moveObjs: MoveObjectsController
@@ -154,52 +140,41 @@ class UIController
         this.clipboard = new Array<string>();
     }
 
-    setActiveTool(tool:Tool)
-    {
-        if(this.activeTool != null)
-        {
+    setActiveTool(tool: Tool) {
+        if (this.activeTool != null) {
             this.activeTool.cancel()
         }
         this.activeTool = tool
     }
 
-    leftClick(pt:Point3d, mesh: BABYLON.Mesh)
-    {
-        if(this.activeTool != null)
-        {
+    leftClick(pt: Point3d, mesh: BABYLON.Mesh) {
+        if (this.activeTool != null) {
             this.activeTool.onMouseDown(pt, mesh)
         }
-        else if(mesh != null)
-        {
+        else if (mesh != null) {
             this.selection.selectObj(mesh)
         }
-        else if(mesh == null)
-        {
+        else if (mesh == null) {
             this.selection.deselectAll();
             gui.guiInstance.clearObjectOverlay();
         }
     }
 
-    rightClick(pt:Point3d, picked: BABYLON.Mesh)
-    {
-        if(this.activeTool != null)
-        {
+    rightClick(pt: Point3d, picked: BABYLON.Mesh) {
+        if (this.activeTool != null) {
             this.activeTool.finish(pt, picked)
             this.activeTool = null
         }
-        else if (picked == null)
-        {
+        else if (picked == null) {
             this.selection.deselectAll();
         }
     }
 
-    mouseMove(pt:Point3d, hovered: BABYLON.Mesh)
-    {
-        if(this.activeTool != null)
-        {
-            if(this.shiftPressed) {
-                if(this.shiftPt) {
-                    if(Math.abs(pt.x - this.shiftPt.x) > Math.abs(pt.y - this.shiftPt.y)) {
+    mouseMove(pt: Point3d, hovered: BABYLON.Mesh) {
+        if (this.activeTool != null) {
+            if (this.shiftPressed) {
+                if (this.shiftPt) {
+                    if (Math.abs(pt.x - this.shiftPt.x) > Math.abs(pt.y - this.shiftPt.y)) {
                         pt = new Point3d(pt.x, this.shiftPt.y, this.shiftPt.z);
                     }
                     else {
@@ -218,75 +193,59 @@ class UIController
         return true;
     }
 
-    objDrag(ev: any, mesh: BABYLON.Mesh)
-    {
-        if(this.activeTool == null)
-        {
+    objDrag(ev: any, mesh: BABYLON.Mesh) {
+        if (this.activeTool == null) {
             this.selection.selectObj(mesh);
             this.moveObjs.move(ev, this.selection.getSelectedObjs());
         }
     }
 
-    objDragEnd(ev: any)
-    {
-        if(this.activeTool == null)
-        {
+    objDragEnd(ev: any) {
+        if (this.activeTool == null) {
             this.moveObjs.endMove(ev);
         }
     }
 
-    ctrlDown()
-    {
+    ctrlDown() {
         this.selection.ctrlPressed = true;
     }
 
-    ctrlUp()
-    {
+    ctrlUp() {
         this.selection.ctrlPressed = false;
     }
 
-    shiftDown()
-    {
+    shiftDown() {
         this.shiftPressed = true;
     }
 
-    shiftUp()
-    {
+    shiftUp() {
         this.shiftPressed = false;
     }
 
-    onDeleteKey()
-    {
-        if(this.activeTool == null)
-        {
+    onDeleteKey() {
+        if (this.activeTool == null) {
             this.selection.deleteSelected();
         }
     }
 
-    cancel()
-    {
-        if(this.activeTool != null)
-        {
+    cancel() {
+        if (this.activeTool != null) {
             this.activeTool.cancel()
             this.activeTool = null
         }
     }
 
-    setClipboard()
-    {
-        if(this.activeTool == null)
-        {
+    setClipboard() {
+        if (this.activeTool == null) {
             this.clipboard = []
-            this.selection.getSelectedObjs().forEach((mesh)=> {
+            this.selection.getSelectedObjs().forEach((mesh) => {
                 this.clipboard.push(mesh.name)
             });
         }
     }
 
-    pasteClipboard()
-    {
-        if(this.activeTool == null)
-        {
+    pasteClipboard() {
+        if (this.activeTool == null) {
             const event = ops.beginUndoEvent("copy objs");
             var copyIdsPromise = ops.copyObjs(event, this.clipboard, new Point3d(20, 0, 0))
             ops.endUndoEvent(event);
@@ -300,11 +259,10 @@ class UIController
     }
 }
 
-export class UIControllerSingleton
-{
+export class UIControllerSingleton {
     private static instance: UIController
     constructor() {
-        if(!UIControllerSingleton.instance) {
+        if (!UIControllerSingleton.instance) {
             UIControllerSingleton.instance = new UIController()
         }
     }
